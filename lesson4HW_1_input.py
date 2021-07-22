0788582eef111476496527e260ffaf2967980c63
Pearltsai <87632978+Pearltsai@users.noreply.github.com>
2021-07-22 14:16:25 +0800

    Add files via upload

---
+++ lesson4HW_1_input.py
@@ -0,0 +1,4 @@
+x=input('number?')
+x=int(x)
+for x in range(1,x):
+    print('*'*x)
---
+++ lesson4HW_2_input.py
@@ -0,0 +1,6 @@
+x=input('number?')
+x=int(x)
+for x in range(1,x+1):
+    print('*'*x)
+for x in range(x-1,0,-1):
+    print('*'*x)
